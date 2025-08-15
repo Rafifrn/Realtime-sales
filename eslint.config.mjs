3ed8c40e195b4eb67b8692b085bc4b12cfd95a4c
Rafifrn <rafizhafran8702@gmail.com>
2025-08-15 22:11:25 +0700

    First commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,4 +13,13 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
+eslintConfig.push({
+  rules: {
+    "react-hooks/rules-of-hooks": "off", // Disable rules of hooks
+    "react/jsx-key": "off", // Disable jsx key rule
+    "@typescript-eslint/no-explicit-any": "off", // Disable explicit any rule
+    "@typescript-eslint/no-unused-vars": "off", // Disable unused vars rule
+  },
+});
+
 export default eslintConfig;
